1b49849481428706174ed5926357841f05de5d6e
Mohit <mr6114671@gmail.com>
2024-06-07 21:57:37 +0530

    protected routes

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -4,7 +4,6 @@ import { Navigate, useNavigate } from 'react-router-dom'
 const ProtectedRoutes = ({children}) => {
 const isAuthenticated=localStorage.getItem('Password') ?true :false
 console.log(isAuthenticated);
-const router=useNavigate()
   return   isAuthenticated ? children : <Navigate to='/'/>
   
 }
